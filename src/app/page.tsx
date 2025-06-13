394c90af08dd8c1e17c432fb7a127caff3222ade
Roland Estropia Jr <restropiajr@gmail.com>
2025-06-13 02:25:24 -0700

    Added layer base css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,12 @@ export default function Home() {
     <div className="mt-[100px]">
       <div className="row-one">
         <div className="col-one flex min-h-screen flex-col items-center justify-center gap-4">
-          <h1 className="text-center text-4xl text-gray-900 md:text-6xl">
-            Welcome to <span className="font-bold tracking-wide text-black">GameExplorer</span>
+          <h1 className="text-center text-4xl text-white md:text-6xl">
+            Welcome to {''}
+            <span className="font-bold tracking-wide text-white">GameExplorer</span>
           </h1>
-          <p className="px-4 text-center text-lg text-gray-700 md:text-xl">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque at autem, non rem quis
-            repellendus explicabo nulla libero error dolor aspernatur ipsa totam debitis, aliquid
-            tempora temporibus accusantium obcaecati quisquam.
+          <p className="px-4 text-center text-lg text-white md:text-xl">
+            An interactive video game archive to explore and review video games!
           </p>
         </div>
       </div>
