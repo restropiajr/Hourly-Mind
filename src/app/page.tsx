9caa6ad37d59722511e7830f7c02270fc69dd66b
Roland Estropia Jr <restropiajr@gmail.com>
2025-06-13 20:58:15 -0700

    Added logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
 'use client'
 
+import Image from 'next/image'
 import { useBreakpoint } from '@/hooks'
-import { Calculator } from 'lucide-react'
 
 export default function Home() {
   const { isMobile } = useBreakpoint()
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
-      <Calculator size={isMobile ? 50 : 75} />
-      <h1 className="text-center">Welcome to Therapay</h1>
-      <p className="text-center">
-        The easiest way for therapists to calculate payroll and manage client billing.
-      </p>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <Image
+        src="/logo.svg"
+        alt="Therapay Logo"
+        width={isMobile ? 100 : 150}
+        height={isMobile ? 100 : 150}
+      />
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-center">Welcome to Therapay</h1>
+        <p className="text-center">
+          The easiest way for therapists to calculate payroll and manage client billing.
+        </p>
+      </div>
     </div>
   )
 }
