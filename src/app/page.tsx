4abe50530e3be5cf23f41784ddf6e01744376e49
Roland Estropia Jr <restropiajr@gmail.com>
2025-06-13 20:21:28 -0700

    Added image logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+'use client'
+
+import { useBreakpoint } from '@/hooks'
+import { Calculator } from 'lucide-react'
+
 export default function Home() {
+  const { isMobile } = useBreakpoint()
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <Calculator size={isMobile ? 50 : 75} />
       <h1 className="text-center">Welcome to Hourly Mind</h1>
       <p className="text-center">
         The easiest way for therapists to calculate payroll and manage client billing.
