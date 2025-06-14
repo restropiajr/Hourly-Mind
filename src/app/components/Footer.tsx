e565df1f35c0ff9be9010c8232d3508961ff1bed
Roland Estropia Jr <restropiajr@gmail.com>
2025-06-13 19:41:42 -0700

    Fixed footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,5 +5,38 @@ type FooterProps = {
 }
 
 export const Footer: FC<FooterProps> = () => {
-  return <div>test</div>
+  return (
+    <footer className="flex h-[160px] w-full items-center justify-center border-t border-white/60 bg-[#FFBFA3]/60">
+      <div className="flex w-2/3 flex-col items-center justify-center gap-4">
+        <p>Copyright &copy; 2025 Roland Estropia Jr.</p>
+        <p>All Rights Reserved</p>
+        <div className="flex gap-2">
+          <a
+            className="link hover-transition"
+            href="https://github.com/restropiajr"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+          <a
+            className="link hover-transition"
+            href="https://www.linkedin.com/in/restropiajr/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            className="link hover-transition"
+            href="https://restropiajr.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Portfolio
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
 }
